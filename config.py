0c4934c8e25f838b0c7ffa73d3aa6f6a5de03394
abolfazl khorshidi <abolfazlkhorshidi2278@gmail.com>
2026-07-26 21:50:32 +0330

    change

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("BOT_TOKEN")
 if not TELEGRAM_TOKEN:
-    raise ValueError("توکن تلگرام در فایل .env یافت نشد!")
+    raise ValueError("توکن تلگرام با نام BOT_TOKEN در فایل .env یافت نشد!")
